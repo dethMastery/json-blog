15aca1431940fcc4d3071469438f5d1601fafbe5
Detzz <a2z.team.198@gmail.com>
2021-06-01 00:04:32 +0700

    blog.php style added

--- blog.php
+++ blog.php
@@ -1,21 +1,66 @@
 <?php
-    $call = file_get_contents('backend/db/blog.php');
-    $decode = json_decode($call);
+$call = file_get_contents('backend/db/blog.json');
+$decode = json_decode($call);
 ?>
 
-้<!DOCTYPE html>
+้
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Content | Detzz.In.Th</title>
+
+    <!-- Main Css -->
+    <link rel="stylesheet" href="asset/blog.css">
+
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@200&display=swap" rel="stylesheet">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 </head>
+
 <body>
     <?php
-        foreach ($decode as $b) {
-            
-        }
+    foreach ($decode as $b) {
     ?>
+        <div class="card" style="width: 18rem;">
+            <?php
+            if ($b->img === "") {
+                echo "<img src='asset/no_img.png' class='card-img-top' alt='header-img'>";
+            } else {
+                echo "<img src='" . $b->img . "' class='card-img-top' alt='header-img'>";
+            }
+            ?>
+            <div class="card-body">
+                <h5 class="card-title">
+                    <center>
+                        <?php
+                            echo $b->title;
+                        ?>
+                    </center>
+                </h5>
+                <p class="card-text">
+                    <?php
+                        echo $b->preview_c;
+                    ?>
+                </p>
+                <form action="read_blog.php">
+                    <input type="text" value="<?php $b->id ?>" class="none">
+                </form>
+
+                <a href="#" class="btn btn-primary">Read more..</a>
+            </div>
+        </div>
+    <?php
+    }
+    ?>
+
+
 </body>
+
 </html>
