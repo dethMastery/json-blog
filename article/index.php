b2d388b739d21f3d5d0e405045ec128e31d942a1
Detzz <a2z.team.198@gmail.com>
2021-06-07 12:36:42 +0700

    added footer

--- article/index.php
+++ article/index.php
@@ -79,17 +79,22 @@ if ($decode[$id]['img'] === "") {
     <header>
         <div class="header-container">
             <?php
-                if ($decode[$id]['youtube-video-id'] === ""){
-                    echo "<div class='blacking'></div>";
-                } else {
-                    echo "<iframe src='https://www.youtube.com/embed/".$decode[$id]['youtube-video-id']."' frameborder='0' allow='accelerometer; encryped-media; picture-in-picture' allowfullscreen></iframe>";
-                };
+            if ($decode[$id]['youtube-video-id'] === "") {
+                echo "<div class='blacking'></div>";
+            } else {
+                echo "<iframe src='https://www.youtube.com/embed/" . $decode[$id]['youtube-video-id'] . "' frameborder='0' allow='accelerometer; encryped-media; picture-in-picture' allowfullscreen></iframe>";
+            };
             ?>
         </div>
     </header>
 
     <!-- Article -->
     <article id="main-article" class="p-5">
+        <span>
+            <button onclick="goBack()" class="btn btn-primary">
+                <-- Back </button>
+        </span>
+        <br><br>
         <h1 class="pb-3">
             <strong>
                 <?php
@@ -107,7 +112,25 @@ if ($decode[$id]['img'] === "") {
         </section>
     </article>
 
+
+
+    <!-- Copyright -->
+    <footer id="copyright" class="p-4 text-center">
+        &copy; <span id="copyright-year"></span> <a href="https://detzz.in.th" target="_blank">Suphakit P.</a> All Right Reserved
+    </footer>
+
     <!-- Script Embed -->
+    <!-- Year -->
+    <script>
+        var today = new Date();
+        var y = today.getFullYear();
+        document.getElementById('copyright-year').innerHTML = y;
+
+        function goBack() {
+            window.history.back();
+        }
+    </script>
+
     <!-- Bootstrap Set up -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
