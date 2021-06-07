b5089be07468437f2b97b0734c8ad8cd525207a4
Detzz <a2z.team.198@gmail.com>
2021-06-07 14:37:48 +0700

    Update index.php

--- backend/index.php
+++ backend/index.php
@@ -1,23 +1,19 @@
 <?php
-$call = file_get_contents('db/main.json');
-$use = json_decode($call);
-
 session_start();
-if (isset($_POST['Username']) && isset($_POST['Password'])) {
-
-    $user = $use[0]['backend']['username'];
-    $pass = $use[0]['backend']['password'];
-    if ($_POST['Username'] == $user && $_POST['Password'] == $pass) {
-        $_SESSION["login"] = true;
-        header("location: /backend/");
-        exit(0);
-    } else {
-        echo "<script>
-        ";
-        echo "alert(\"Username or Password is invalid\");";
-        echo "window.history.back()";
-        echo "
-    </script>";
-    }
+if (!isset($_SESSION["login"]) && $_SESSION["login"] != true) {
+    header("location: login.php");
+    exit(0);
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Backend</title>
+</head>
+<body>
+    
+</body>
+</html>
