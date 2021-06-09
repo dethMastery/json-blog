3091390063890c0d483424deed677e4eaf5eb4d4
Detzz <a2z.team.198@gmail.com>
2021-06-09 10:16:50 +0700

    added temp editor page

--- backend/b-blog-edit.php
+++ backend/b-blog-edit.php
@@ -5,5 +5,19 @@ if (!isset($_SESSION["login"]) && $_SESSION["login"] != true) {
     exit(0);
 }
 
+$id = $_POST['id'];
+// read file
+$data = file_get_contents('db/blog.json');
 
+// decode json to array
+$json_arr = json_decode($data, true);
+
+foreach ($json_arr as $key => $value) {
+    if ($value['id'] == $id) {
+        $json_arr[$key]['Sports'] = "Foot Ball";
+    }
+}
+
+// encode array to json and save to file
+file_put_contents('db/blog.json', json_encode($json_arr));
 ?>
--- backend/b-blog.php
+++ backend/b-blog.php
@@ -1,9 +1,9 @@
 <?php
-                            session_start();
-                            if (!isset($_SESSION["login"]) && $_SESSION["login"] != true) {
-                                header("location: ../backend/login.php");
-                                exit(0);
-                            }
+session_start();
+if (!isset($_SESSION["login"]) && $_SESSION["login"] != true) {
+    header("location: ../backend/login.php");
+    exit(0);
+}
 
 $main = file_get_contents('db/main.json');
 $m_call = json_decode($main, true);
