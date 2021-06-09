118ef52c1ac2f2e967a9e5883048a42969610d20
Detzz <a2z.team.198@gmail.com>
2021-06-09 09:23:31 +0700

    delete system added

--- backend/b-blog-del.php
+++ backend/b-blog-del.php
@@ -1,12 +1,32 @@
 <?php
-    session_start();
-    if (!isset($_SESSION["login"]) && $_SESSION["login"] != true) {
-        header("location: ../backend/login.php");
-        exit(0);
+session_start();
+if (!isset($_SESSION["login"]) && $_SESSION["login"] != true) {
+    header("location: ../backend/login.php");
+    exit(0);
+}
+
+$id = $_POST['id'];
+
+$get = file_get_contents('db/blog.json');
+$json_arr = json_decode($get, true);
+
+$arr_index = array();
+foreach ($json_arr as $key => $value) {
+    if ($value['id'] == $id) {
+        $arr_index[] = $key;
     }
-    
-    $id = $_POST[''];
+}
 
-    $get = file_get_contents('db/blog.json');
+foreach ($arr_index as $i) {
+    unset($json_arr[$i]);
+}
 
+$json_arr = array_values($json_arr);
+
+file_put_contents('db/blog.json', json_encode($json_arr));
+
+echo "<script>";
+echo "alert(\"UwU finished delete your data\");";
+echo "window.history.back()";
+echo "</script>";
 ?>
